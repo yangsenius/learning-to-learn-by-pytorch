71db51c90b5bc32ab187866f399bd5f91619ae0b
senius <465097485@qq.com>
2018-12-28 17:14:58 +0800

    Update Simple_LSTM.py

--- Simple_LSTM.py
+++ Simple_LSTM.py
@@ -1,27 +1,27 @@
 class Simple_LSTM_Optimizer(torch.nn.Module):
-
-    def __init__(self, input_dim, hidden_dim, num_stacks, output_dim ,batchsize):
+    
+    def __init__(self, args):
         super(LSTM_Optimizer,self).__init__()
-        self.batchsize = batchsize
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
-        self.num_stacks = num_stacks
-	### LSTM模块 ##
+        input_dim, hidden_dim, num_stacks, output_dim = args()
+	
+	# LSTM 模块 
         self.lstm = torch.nn.LSTM(input_dim, hidden_dim, num_stacks)
         self.Linear = torch.nn.Linear(hidden_dim, output_dim)
 
     def Output_And_Update(self, input_gradients, prev_state):
     
         if prev_state is None: #init_state
-            cell = torch.zeros(self.num_stacks,self.batchsize,self.hidden_dim)
-            hidden = torch.zeros(self.num_stacks,self.batchsize,self.hidden_dim)
-	## LSTM 更新cell和hidden，并输出hidden  ##
+           (cell , hidden) = prev_state.init()
+	
+	# LSTM 更新cell和hidden，并输出hidden 
         update , (cell,hidden) = self.lstm(input_gradients, (cell, hidden))
         update = self.Linear(update) 
+	
         return update, (cell, hidden)
     	
     def forward(self,input_gradients, prev_state):
-	##输入梯度，输出预测梯度 并更新隐状态
-        update , next_state = self.Output_And_Update (input_gradients , prev_state)  
-        return update , next_state
+	
+	# 输入梯度，输出预测梯度 并更新隐状态
+        update , next_state = self.Output_And_Update (input_gradients , prev_state) 
+	
+	return update , next_state
