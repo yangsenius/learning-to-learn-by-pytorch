5d54ec6a8a4098e01e23332a8927652d3f4c720c
senius <465097485@qq.com>
2019-01-21 23:07:43 +0800

    add some English annotation

--- learning_to_learn.py
+++ learning_to_learn.py
@@ -7,12 +7,15 @@
 # https://yangsenius.github.io/blog/LSTM_Meta/
 # author：yangsen
 # #### “通过梯度下降来学习如何通过梯度下降学习”
+# #### "learning to learn by gradient descent by gradient descent"
 # #### 要让优化器学会这样   "为了更好地得到，要先去舍弃"  这样类似的知识！
+# #### make the optimizer to learn the knowledge of "sometimes, in order to get it better, you have to give up first. "
 
 import torch
 import torch.nn as nn
 from timeit import default_timer as timer
 #####################      优化问题   ##########################
+#####################      optimization  ##########################
 USE_CUDA = False
 DIM = 10
 batchsize = 128
@@ -37,6 +40,7 @@ def f(W,Y,x):
 ###############################################################
 
 ######################    手工的优化器   ###################
+######################    hand-craft optimizer   ###################
 
 def SGD(gradients, state, learning_rate=0.001):
    
@@ -59,8 +63,10 @@ def adam():
 
 
 #####################    自动 LSTM 优化器模型  ##########################
+#####################    auto LSTM optimizer model  ##########################
 class LSTM_Optimizee_Model(torch.nn.Module):
-    """LSTM优化器"""
+    """LSTM优化器
+       LSTM optimizer"""
     
     def __init__(self,input_size,output_size, hidden_size, num_stacks, batchsize, preprocess = True ,p = 10 ,output_scale = 1):
         super(LSTM_Optimizee_Model,self).__init__()
@@ -69,7 +75,7 @@ class LSTM_Optimizee_Model(torch.nn.Module):
         self.input_flag = 2
         if preprocess != True:
              self.input_flag = 1
-        self.output_scale = output_scale #论文
+        self.output_scale = output_scale 
         self.lstm = torch.nn.LSTM(input_size*self.input_flag, hidden_size, num_stacks)
         self.Linear = torch.nn.Linear(hidden_size,output_size) #1-> output_size
         
@@ -87,10 +93,10 @@ class LSTM_Optimizee_Model(torch.nn.Module):
         log = torch.log(torch.abs(gradients))
         clamp_log = torch.clamp(log/p , min = -1.0,max = 1.0)
         clamp_sign = torch.clamp(torch.exp(torch.Tensor(p))*gradients, min = -1.0, max =1.0)
-        return torch.cat((clamp_log,clamp_sign),dim = -1) #在gradients的最后一维input_dims拼接
+        return torch.cat((clamp_log,clamp_sign),dim = -1) #在gradients的最后一维input_dims拼接 # concatenate in final dim
     
     def Output_Gradient_Increment_And_Update_LSTM_Hidden_State(self, input_gradients, prev_state):
-        """LSTM的核心操作
+        """LSTM的核心操作  core operation
         coordinate-wise LSTM """
         if prev_state is None: #init_state
             prev_state = (torch.zeros(Layers,batchsize,Hidden_nums),
@@ -100,14 +106,14 @@ class LSTM_Optimizee_Model(torch.nn.Module):
                             torch.zeros(Layers,batchsize,Hidden_nums).cuda())
          			
         update , next_state = self.lstm(input_gradients, prev_state)
-        update = self.Linear(update) * self.output_scale #因为LSTM的输出是当前步的Hidden，需要变换到output的相同形状上 
+        update = self.Linear(update) * self.output_scale # transform the LSTM output to the target output dim 
         return update, next_state
     
     def forward(self,input_gradients, prev_state):
         if USE_CUDA:
             input_gradients = input_gradients.cuda()
-        #LSTM的输入为梯度，pytorch要求torch.nn.lstm的输入为（1，batchsize,input_dim）
-        #原gradient.size()=torch.size[5] ->[1,1,5]
+        #pytorch requires the `torch.nn.lstm`'s input as（1，batchsize,input_dim）
+        # original gradient.size()=torch.size[5] ->[1,1,5]
         gradients = input_gradients.unsqueeze(0)
       
         if self.preprocess_flag == True:
@@ -120,6 +126,7 @@ class LSTM_Optimizee_Model(torch.nn.Module):
         return update , next_state
     
 #################   优化器模型参数  ##############################
+#################   Parameters of optimizer ##############################
 Layers = 2
 Hidden_nums = 20
 Input_DIM = DIM
@@ -127,6 +134,7 @@ Output_DIM = DIM
 output_scale_value=1
 
 #######   构造一个优化器  #######
+#######  construct a optimizer  #######
 LSTM_Optimizee = LSTM_Optimizee_Model(Input_DIM, Output_DIM, Hidden_nums ,Layers , batchsize=batchsize,\
                 preprocess=False,output_scale=output_scale_value)
 print(LSTM_Optimizee)
@@ -136,17 +144,17 @@ if USE_CUDA:
    
 
 ######################  优化问题目标函数的学习过程   ###############
-
+######################  the learning process of optimizing the target function   ###############
 
 class Learner( object ):
     """
     Args :
-        `f` : 要学习的问题
-        `optimizee` : 使用的优化器
-        `train_steps` : 对于其他SGD,Adam等是训练周期，对于LSTM训练时的展开周期
-        `retain_graph_flag=False`  : 默认每次loss_backward后 释放动态图
-        `reset_theta = False `  :  默认每次学习前 不随机初始化参数
-        `reset_function_from_IID_distirbution = True` : 默认从分布中随机采样函数 
+        `f` : 要学习的问题 the learning problem
+        `optimizee` : 使用的优化器 the used optimizer 
+        `train_steps` : 对于其他SGD,Adam等是训练周期，对于LSTM训练时的展开周期  training steps for SGD and ADAM, unfolded step for LSTM train
+        `retain_graph_flag=False`  : 默认每次loss_backward后 释放动态图 default: free the dynamic graph after the loss backward
+        `reset_theta = False `  :  默认每次学习前 不随机初始化参数 default: do not initialize the theta
+        `reset_function_from_IID_distirbution = True` : 默认从分布中随机采样函数  default: random sample from  distribution
 
     Return :
         `losses` : reserves each loss value in each iteration
@@ -168,14 +176,15 @@ class Learner( object ):
         self.init_theta_of_f()
         self.state = None
 
-        self.global_loss_graph = 0 #这个是为LSTM优化器求所有loss相加产生计算图准备的
-        self.losses = []   # 保存每个训练周期的loss值
+        self.global_loss_graph = 0 # global loss for optimizing LSTM
+        self.losses = []   # KEEP each loss of all epoches
 
     def init_theta_of_f(self,):  
-        ''' 初始化 优化问题 f 的参数 '''
+        ''' 初始化 优化问题 f 的参数 
+            initialize the theta of optimization f '''
         self.DIM = 10
         self.batchsize = 128
-        self.W = torch.randn(batchsize,DIM,DIM) #代表 已知的数据 # 独立同分布的标准正太分布
+        self.W = torch.randn(batchsize,DIM,DIM) # represents IID
         self.Y = torch.randn(batchsize,DIM)
         self.x = torch.zeros(self.batchsize,self.DIM)
         self.x.requires_grad = True
@@ -201,9 +210,10 @@ class Learner( object ):
             
             
         ################   每次全局训练迭代，从独立同分布的Normal Gaussian采样函数     ##################
+        ################   at the first iteration , sample from IID Normal Gaussian    ##################
         if num_roll == 0 and reset_function_from_IID_distirbution == True :
-            W = torch.randn(batchsize,DIM,DIM) #代表 已知的数据 # 独立同分布的标准正太分布
-            Y = torch.randn(batchsize,DIM)     #代表 数据的标签 #  独立同分布的标准正太分布
+            W = torch.randn(batchsize,DIM,DIM) # represents IID
+            Y = torch.randn(batchsize,DIM)     # represents IID
          
             
         if num_roll == 0:
@@ -223,11 +233,11 @@ class Learner( object ):
         '''
         Total Training steps = Unroll_Train_Steps * the times of  `Learner` been called
         
-        SGD,RMS,LSTM 用上述定义的
-         Adam优化器直接使用pytorch里的，所以代码上有区分 后面可以完善！'''
+        SGD,RMS,LSTM FROM defination above
+         but Adam is adopted by pytorch~ This can be improved later'''
         f  = self.f 
         x , W , Y , state =  self.Reset_Or_Reuse(self.x , self.W , self.Y , self.state , num_roll )
-        self.global_loss_graph = 0   #每个unroll的开始需要 重新置零
+        self.global_loss_graph = 0   #at the beginning of unroll, reset to 0 
         optimizee = self.optimizee
         print('state is None = {}'.format(state == None))
      
@@ -238,7 +248,7 @@ class Learner( object ):
                 #self.global_loss_graph += (0.8*torch.log10(torch.Tensor([i+1]))+1)*loss
                 self.global_loss_graph += loss
               
-                loss.backward(retain_graph=self.retain_graph_flag) # 默认为False,当优化LSTM设置为True
+                loss.backward(retain_graph=self.retain_graph_flag) # default as False,set to True for LSTMS
               
                 update, state = optimizee(x.grad, state)
               
@@ -274,6 +284,7 @@ class Learner( object ):
 
 
 #######   LSTM 优化器的训练过程 Learning to learn   ###############
+#######   LSTM training Learning to learn   ###############
 
 def Learning_to_learn_global_training(optimizee, global_taining_steps, Optimizee_Train_Steps, UnRoll_STEPS, Evaluate_period ,optimizer_lr=0.1):
     """ Training the LSTM optimizee . Learning to learn
@@ -290,7 +301,8 @@ def Learning_to_learn_global_training(optimizee, global_taining_steps, Optimizee
 
     LSTM_Learner = Learner(f, optimizee, UnRoll_STEPS, retain_graph_flag=True, reset_theta=True,)
   #这里考虑Batchsize代表IID的话，那么就可以不需要每次都重新IID采样
-  #即reset_function_from_IID_distirbution = False 否则为True
+  # If regarding `Batchsize` as `IID` ,there is no need for reset the theta
+  #That is ，reset_function_from_IID_distirbution = False else it is True
 
     best_sum_loss = 999999
     best_final_loss = 999999
@@ -347,7 +359,7 @@ def evaluate(best_sum_loss,best_final_loss, best_flag,lr):
         
     return best_sum_loss, best_final_loss, best_flag 
 
-    #############  注意：接上一片段的代码！！   #######################3#
+    
 ##########################   before learning LSTM optimizee ###############################
 import numpy as np
 import matplotlib
@@ -356,7 +368,7 @@ import matplotlib.pyplot as plt
 STEPS = 100
 x = np.arange(STEPS)
 
-Adam = 'Adam' #因为这里Adam使用Pytorch
+Adam = 'Adam' # Adam in Pytorch
 
 for _ in range(1): 
    
@@ -384,13 +396,13 @@ for _ in range(1):
     print("\n\nsum_loss:sgd={},rms={},adam={},lstm={}".format(sgd_sum_loss,rms_sum_loss,adam_sum_loss,lstm_sum_loss ))
 
 
-#######   注意：接上一段的代码！！
-#################### Learning to learn (优化optimizee) ######################
-Global_Train_Steps = 1000 #可修改
+
+#################### Learning to learn (optimizing optimizee) ######################
+Global_Train_Steps = 1000 #可修改 changeable
 Optimizee_Train_Steps = 100
 UnRoll_STEPS = 20
-Evaluate_period = 1 #可修改
-optimizer_lr = 0.1 #可修改
+Evaluate_period = 1 #可修改 changeable
+optimizer_lr = 0.1 #可修改 changeable
 global_loss_list ,flag = Learning_to_learn_global_training(   LSTM_Optimizee,
                                                         Global_Train_Steps,
                                                         Optimizee_Train_Steps,
@@ -412,7 +424,7 @@ global_loss_list ,flag = Learning_to_learn_global_training(   LSTM_Optimizee,
 #plt.title('Training LSTM optimizee by gradient descent ')
 #plt.show()
 
-###############  **注意： **接上一片段的代码****
+
 ######################################################################3#
 ##########################   show contrast results SGD,ADAM, RMS ,LSTM ###############################
 import copy
@@ -431,9 +443,9 @@ LSTM_Optimizee.load_state_dict(torch.load('best_LSTM_optimizer.pth'))
 STEPS = 100
 x = np.arange(STEPS)
 
-Adam = 'Adam' #因为这里Adam使用Pytorch
+Adam = 'Adam'
 
-for _ in range(3): #可以多试几次测试实验，LSTM不稳定
+for _ in range(3): #可以多试几次测试实验，LSTM不稳定 for several test,  the trained LSTM is not stable?
     
     SGD_Learner = Learner(f , SGD, STEPS, eval_flag=True,reset_theta=True,)
     RMS_Learner = Learner(f , RMS, STEPS, eval_flag=True,reset_theta=True,)
