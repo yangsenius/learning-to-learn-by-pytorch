1d568ad09219ec77c39283d0da4cb61f022277c9
senius <465097485@qq.com>
2019-03-26 23:59:50 +0800

    consideration for LSTM's unrolled step(such as 20)
    such as unrolled step = 20, T =100, TT=10000

--- simple_learning_to_learn.py
+++ simple_learning_to_learn.py
@@ -1,4 +1,4 @@
-def learning_to_learn(f, x, LSTM_Optimizer, Adam_Optimizer ,T ,TT):
+def learning_to_learn(f, x, LSTM_Optimizer, Adam_Optimizer ,T ,TT, unrolled_step):
 
     for tt in range(TT):
     
@@ -8,15 +8,18 @@ def learning_to_learn(f, x, LSTM_Optimizer, Adam_Optimizer ,T ,TT):
             
             loss = f(x)
 
-            x_grad = loss.backward()
+            x_grad = loss.backward(retain_graph=True)
 
             x += LSTM_Optimizer(x_grad)
 
             L += loss
             
+            if (t+1)% unrolled_step ==0:
 
-        LSTM_grad = L.backward()
+                LSTM_grad = L.backward()
 
-        LSTM_Optimizer.parameters += Adam_Optimizer( LSTM_grad)
+                LSTM_Optimizer.parameters += Adam_Optimizer( LSTM_grad)
+                
+                L = 0
 
     return LSTM_Optimizer
